72be8d142d51f448783393457441964ce9116ddd
vinhhao-truong <vinhhao.truong.52@gmail.com>
2021-04-25 01:08:43 +1000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './assets/scss/custom.scss';
+
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storageItems) {
+      setTodos(storageItems)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo])
+  }
+
+  const toggle = (id) => {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo;
+      })
+    )
+  }
+
+  const checkOrUncheckAll = (status) => {
+    setTodos(
+      todos.map(todo => {
+        return {
+          ...todo,
+          completed: status
+        }
+      })
+    )
+  }
+
+  const remove = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  const removeChecked = () => {
+    setTodos(todos.filter(todo => todo.completed === false));
+  }
+
+  const removeAll = () => {
+    setTodos([]);
+  }
+
+  const edit = (id, newContent) => {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id) {
+          return {
+            ...newContent
+          }
+        }
+        return todo;
+      })
+    )       
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        toggle={toggle}
+        checkOrUncheckAll={checkOrUncheckAll}
+        remove={remove}
+        removeChecked={removeChecked}
+        removeAll={removeAll}
+        edit={edit}
+      />
     </div>
   );
 }
